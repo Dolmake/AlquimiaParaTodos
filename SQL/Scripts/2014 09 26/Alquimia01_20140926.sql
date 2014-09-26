c09169113e9d15b59c26a97b8ee6451906feb587
Dolmake <demondolmake@hotmail.com>
2014-09-26 13:13:24 +0200

    SQL creation scripts

---
+++ SQL/Scripts/2014 09 26/Alquimia01_20140926.sql
@@ -0,0 +1,247 @@
+﻿USE [db2622193_MEZCLA_01]
+GO
+/****** Object:  Table [dbo].[__MigrationHistory]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[__MigrationHistory](
+	[MigrationId] [nvarchar](150) NOT NULL,
+	[ContextKey] [nvarchar](300) NOT NULL,
+	[Model] [varbinary](max) NOT NULL,
+	[ProductVersion] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK_dbo.__MigrationHistory] PRIMARY KEY CLUSTERED 
+(
+	[MigrationId] ASC,
+	[ContextKey] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[LastName] [nvarchar](max) NULL,
+	[Email] [nvarchar](max) NULL,
+	[IsClient] [bit] NOT NULL,
+	[UserName] [nvarchar](max) NULL,
+	[Password] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.User] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Option]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Option](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Color] [nvarchar](max) NULL,
+	[Description] [nvarchar](max) NULL,
+	[ExtraPrice] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_dbo.Option] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Course]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Course](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[Description] [nvarchar](max) NULL,
+	[StartDate] [datetime] NOT NULL,
+	[EndDate] [datetime] NOT NULL,
+	[Hours] [decimal](18, 2) NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_dbo.Course] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Category]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Category](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.Category] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Order]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Order](
+	[ID] [int] NOT NULL,
+	[UserID] [int] NOT NULL,
+	[Code] [nvarchar](max) NULL,
+	[Date] [datetime] NOT NULL,
+	[Total] [decimal](18, 2) NOT NULL,
+	[ExtraPrice] [decimal](18, 2) NOT NULL,
+	[State] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.Order] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC,
+	[UserID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Product]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](max) NULL,
+	[Inci] [nvarchar](max) NULL,
+	[Summary] [nvarchar](max) NULL,
+	[SliderImageUrl] [nvarchar](max) NULL,
+	[ImageUrl] [nvarchar](max) NULL,
+	[ImageDescription] [nvarchar](max) NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[BasePrice] [decimal](18, 2) NOT NULL,
+	[Weight] [decimal](18, 2) NOT NULL,
+	[Stock] [int] NOT NULL,
+	[Offline] [bit] NOT NULL,
+	[ProductOption_ID] [int] NULL,
+	[Product_ID] [int] NULL,
+	[Order_ID] [int] NULL,
+	[Order_UserID] [int] NULL,
+ CONSTRAINT [PK_dbo.Product] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ProductCourse]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProductCourse](
+	[ProductID] [int] NOT NULL,
+	[CourseID] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.ProductCourse] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC,
+	[CourseID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Cart]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cart](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[CartOwnerID] [nvarchar](max) NULL,
+	[Quantity] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[DateCreated] [datetime] NOT NULL,
+ CONSTRAINT [PK_dbo.Cart] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CategoryProduct]    Script Date: 09/26/2014 12:56:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CategoryProduct](
+	[CategoryID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.CategoryProduct] PRIMARY KEY CLUSTERED 
+(
+	[CategoryID] ASC,
+	[ProductID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  ForeignKey [FK_dbo.Cart_dbo.Product_ProductID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[Cart]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Cart_dbo.Product_ProductID] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Product] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Cart] CHECK CONSTRAINT [FK_dbo.Cart_dbo.Product_ProductID]
+GO
+/****** Object:  ForeignKey [FK_dbo.CategoryProduct_dbo.Category_CategoryID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[CategoryProduct]  WITH CHECK ADD  CONSTRAINT [FK_dbo.CategoryProduct_dbo.Category_CategoryID] FOREIGN KEY([CategoryID])
+REFERENCES [dbo].[Category] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[CategoryProduct] CHECK CONSTRAINT [FK_dbo.CategoryProduct_dbo.Category_CategoryID]
+GO
+/****** Object:  ForeignKey [FK_dbo.CategoryProduct_dbo.Product_ProductID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[CategoryProduct]  WITH CHECK ADD  CONSTRAINT [FK_dbo.CategoryProduct_dbo.Product_ProductID] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Product] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[CategoryProduct] CHECK CONSTRAINT [FK_dbo.CategoryProduct_dbo.Product_ProductID]
+GO
+/****** Object:  ForeignKey [FK_dbo.Order_dbo.User_UserID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Order_dbo.User_UserID] FOREIGN KEY([UserID])
+REFERENCES [dbo].[User] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_dbo.Order_dbo.User_UserID]
+GO
+/****** Object:  ForeignKey [FK_dbo.Product_dbo.Option_ProductOption_ID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Product_dbo.Option_ProductOption_ID] FOREIGN KEY([ProductOption_ID])
+REFERENCES [dbo].[Option] ([ID])
+GO
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_dbo.Product_dbo.Option_ProductOption_ID]
+GO
+/****** Object:  ForeignKey [FK_dbo.Product_dbo.Order_Order_ID_Order_UserID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Product_dbo.Order_Order_ID_Order_UserID] FOREIGN KEY([Order_ID], [Order_UserID])
+REFERENCES [dbo].[Order] ([ID], [UserID])
+GO
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_dbo.Product_dbo.Order_Order_ID_Order_UserID]
+GO
+/****** Object:  ForeignKey [FK_dbo.Product_dbo.Product_Product_ID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Product_dbo.Product_Product_ID] FOREIGN KEY([Product_ID])
+REFERENCES [dbo].[Product] ([ID])
+GO
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_dbo.Product_dbo.Product_Product_ID]
+GO
+/****** Object:  ForeignKey [FK_dbo.ProductCourse_dbo.Course_CourseID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[ProductCourse]  WITH CHECK ADD  CONSTRAINT [FK_dbo.ProductCourse_dbo.Course_CourseID] FOREIGN KEY([CourseID])
+REFERENCES [dbo].[Course] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[ProductCourse] CHECK CONSTRAINT [FK_dbo.ProductCourse_dbo.Course_CourseID]
+GO
+/****** Object:  ForeignKey [FK_dbo.ProductCourse_dbo.Product_ProductID]    Script Date: 09/26/2014 12:56:17 ******/
+ALTER TABLE [dbo].[ProductCourse]  WITH CHECK ADD  CONSTRAINT [FK_dbo.ProductCourse_dbo.Product_ProductID] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Product] ([ID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[ProductCourse] CHECK CONSTRAINT [FK_dbo.ProductCourse_dbo.Product_ProductID]
+GO
